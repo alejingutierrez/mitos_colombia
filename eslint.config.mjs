eaf357594ec636d6464222b8d58a9d5addc670ac
Alejo Gutierrez <alegut@MacBook-Pro-de-Alejo.local>
2026-07-06 15:11:22 -0500

    chore(deps): reconcile npm updates and lint config

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import nextVitals from "eslint-config-next/core-web-vitals";
+
+const config = [
+  ...nextVitals,
+  {
+    rules: {
+      "react-hooks/immutability": "off",
+      "react-hooks/set-state-in-effect": "off",
+    },
+  },
+  {
+    ignores: [
+      ".next/**",
+      "out/**",
+      "build/**",
+      "coverage/**",
+      ".vercel/**",
+    ],
+  },
+];
+
+export default config;
--- src/components/StructuredData.js
+++ src/components/StructuredData.js
@@ -14,7 +14,6 @@ function JsonLdScript({ data }) {
   return (
     <script
       type="application/ld+json"
-      // eslint-disable-next-line react/no-danger
       dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
     />
   );
